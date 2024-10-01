fe8c34e1625b960c06091a176db105f4c42ac6bb
Elnama <93837085+ielnama@users.noreply.github.com>
2024-10-01 16:41:20 +0400

    updates on exercise 9

--- week3/ex9.c
+++ week3/ex9.c
@@ -11,9 +11,13 @@
  *	series for sine and use n as the number of terms to be accumulated. 
  *
  *	Solution:
- *
- *	---
- *
+ *  Declare a variable term for calculating each term and sinx which stores the sum of the terms
+ *  compute i=0 as a base case for sinx and term.
+ *  In a while loop compute (i)^(-1) by multiplying sign with -1 in each iteration
+ *  compute the term by multiplying it with (x * x) /((2*i)*(2 * i + 1)) in each iteration
+ * find the sum by adding the term multiplied with the sign in each iteration
+ * iterate the counter
+ * print your computed sum and also sin(x) computed by math.h function to see their comparison 
  */
 
 #include <stdio.h>
@@ -21,14 +25,14 @@
 
 int main() {
 	double x, sinx, term;
-	int n, i = 2;
+	int n, i = 1;
 	scanf("%lf %d", &x, &n);
 
 	sinx = term = x;
 	int sign = 1;
 
 	while (i <= n) {
-		term = (term * x * x) / ((2 * i - 1) * (2 * i - 2));
+		term = (term * x * x) / ((2 * i + 1) * (2 * i));
 		sign = -sign;
 		sinx += sign * term;
 		i++;
