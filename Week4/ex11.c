1f79f1555ed495e2a2a34d898c3ca9c950157049
Elnama <93837085+ielnama@users.noreply.github.com>
2024-10-07 20:55:21 +0400

    update

--- Week4/ex11.c
+++ Week4/ex11.c
@@ -4,18 +4,14 @@
  *
  *	Problem:
  *
- *  A positive integer is given. Write a program to check whether the number is Armstrong number or not.
- *  A number is called Armstrong number when then number is equal to the sum of its own digits each raised
- *  to the power of the number of digits.
+ *	Given a sequence of integers. Find the sum of even and sum of odd elements
+ *                    with using a single repetition and print them separately.
  *
  *
  *	Solution:
  *
- *	Use a for loop to count the number of digits.
- *  Use another for loop to calculate the sum of digits each raised to the power of digitCount
- *  Check if the calculated sum equals the input value
+ *	Use a single for loop to input the numbers and add them to the appropriate sum variable (either even or odd).
  */
-
 #include <stdio.h>
 #include <math.h>
 
