a789538fda09d0e2d52bedc0f1fb75e86ace2ef5
Elnama <93837085+ielnama@users.noreply.github.com>
2024-10-07 20:56:33 +0400

    update

--- Week4/ex11.c
+++ Week4/ex11.c
@@ -4,38 +4,42 @@
  *
  *	Problem:
  *
- *	Given a sequence of integers. Find the sum of even and sum of odd elements
- *                    with using a single repetition and print them separately.
+ *  A positive integer is given. Write a program to check whether the number is Armstrong number or not.
+ *  A number is called Armstrong number when then number is equal to the sum of its own digits each raised
+ *  to the power of the number of digits.
  *
  *
  *	Solution:
  *
- *	Use a single for loop to input the numbers and add them to the appropriate sum variable (either even or odd).
+ *	Use a for loop to count the number of digits.
+ *  Use another for loop to calculate the sum of digits each raised to the power of digitCount
+ *  Check if the calculated sum equals the input value
  */
+
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
-    int num, currentDigit, powerSum = 0, cnt = 0;
-    scanf("%d", &num);
-    for (int i = num; i > 0; i /= 10)
-    {
-        cnt++;
-    }
-    for (int i = num; i > 0; i /= 10)
-    {
-        currentDigit = i % 10;
-        powerSum += pow(currentDigit, cnt);
-    }
-    if (powerSum == num)
-    {
-        printf("YES\n");
-    }
-    else
-    {
-        printf("NO\n");
-    }
+	int num, currentDigit, powerSum = 0, cnt = 0;
+	scanf("%d", &num);
+	for (int i = num; i > 0; i /= 10)
+	{
+		cnt++;
+	}
+	for (int i = num; i > 0; i /= 10)
+	{
+		currentDigit = i % 10;
+		powerSum += pow(currentDigit, cnt);
+	}
+	if (powerSum == num)
+	{
+		printf("YES\n");
+	}
+	else
+	{
+		printf("NO\n");
+	}
 
-    return 0;
+	return 0;
 }
