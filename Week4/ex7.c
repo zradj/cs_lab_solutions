18720f5ff89d5d7d6418aa583ad80bf5c55238c3
Elnama <93837085+ielnama@users.noreply.github.com>
2024-10-08 13:44:51 +0400

    update

--- Week4/ex7.c
+++ Week4/ex7.c
@@ -1,37 +1,37 @@
 /*
  *
- *  Author: Elnama Mammadova
+ *	Author: Elnama Mammadova
  *
- *  Problem:
+ *	Problem:
  *
- *  Given a sequence of integers. Find the sum of even and sum of odd elements
- *  with using a single repetition and print them separately.
+ *	Given a sequence of integers. Find the sum of even and sum of odd elements
+ *	with using a single repetition and print them separately.
  *
  *
- *  Solution:
+ *	Solution:
  *
- *  Use a single for loop to input the numbers and add them to the appropriate sum variable (either even or odd).
+ *	Use a single for loop to input the numbers and add them to the appropriate sum variable (either even or odd).
  */
 #include <stdio.h>
 
 int main()
 {
-    int num;
-    int sum_even = 0, sum_odd = 0;
-    for (; scanf("%d", &num) != EOF;)
-    {
-        if (num % 2 == 0)
-        {
-            sum_even += num;
-        }
-        else
-        {
-            sum_odd += num;
-        }
-    }
+	int num;
+	int sum_even = 0, sum_odd = 0;
+	for (; scanf("%d", &num) != EOF;)
+	{
+		if (num % 2 == 0)
+		{
+			sum_even += num;
+		}
+		else
+		{
+			sum_odd += num;
+		}
+	}
 
-    printf("%d\n", sum_even);
-    printf("%d\n", sum_odd);
+	printf("%d\n", sum_even);
+	printf("%d\n", sum_odd);
 
-    return 0;
+	return 0;
 }
