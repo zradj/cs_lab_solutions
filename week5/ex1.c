df399165da4fcb0a7a2c5034326dcab71bea6608
Elnama <93837085+ielnama@users.noreply.github.com>
2024-10-15 19:59:45 +0400

    odd-numbered exercises

---
+++ week5/ex1.c
@@ -0,0 +1,36 @@
+/*
+ *
+ *	Author: Elnama Mammadova
+ *
+ *	Problem:
+ *
+ *	Given an array of integers. Write a program that takes an integer n and followed
+ *	by n number of integers and print the elements that are less than the average of the array.
+ *
+ *	Solution:
+ *
+ *	Input the number of elements n in the array. Declare an array of size n.
+ *	Use a for loop to input the elements of the array. As you input each element, add it to the sum variable.
+ *	Calculate the average using the formula average = sum / n.
+ *	Use another for loop to iterate through the array. For each element, check if it is less than the average. If the condition holds, print that element.
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+	int n;
+	float sum = 0;
+	scanf("%d", &n);
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+		sum += arr[i];
+	}
+	float avg = sum / n;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < avg) {
+			printf("%d ", arr[i]);
+		}
+	}
+}
---
+++ week5/ex11.c
@@ -0,0 +1,49 @@
+/*
+ *
+ *	Author: Elnama Mammadova
+ *
+ *	Problem:
+ *
+ *	Given an array of integers. Write a program that takes an integer n
+ *  and followed by n number of integers. It is guaranteed that all the numbers are repeated at 
+ *  least twice except one. You need to find that single element that is not repeated.
+ *  Note: All the elements of the array are in [1-1000] range.
+ *
+ *	Solution:
+ *
+ *	Read the number n.
+ *	Declare an array of size n to store the integers.
+ *	Declare a count array of size 1001 to track the occurrences of integers in the input, 
+ *	as the numbers are in the range [1-1000].
+ *	Initialize the count array with zeros for all elements from index 1 to 1000.
+ *	This array will hold the count of each element in the original array, by storing the count in cnt[number].
+ *	Therefore, if number = arr[i], then cnt[arr[i]] will hold the count of arr[i].
+ *	Input the elements of the array; for each integer read, increment its corresponding 
+ *	count in the count array.
+ *	Loop through the original array to find the first integer that occurs exactly once.
+ *	If such an integer is found, print it and break out of the loop.
+ *
+ */
+
+
+#include<stdio.h>
+
+int main() {
+ int n;
+ scanf("%d",&n);
+ int arr[n];
+ int cnt[1001];
+  for(int i=1;i<1001;i++) {
+	cnt[i]=0;
+ }
+ for(int i=0;i<n;i++) {
+	scanf("%d",&arr[i]);
+	cnt[arr[i]]++;
+ }
+ for(int i=0;i<n;i++) {
+	if(cnt[arr[i]]==1) {
+		printf("%d",arr[i]);
+		break;
+	}
+ }
+}
