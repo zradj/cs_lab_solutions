cde6de3b327b1ce5bdfc480a95de423c08d3b63d
Zaur Rajabov <zradj7@gmail.com>
2024-10-16 23:44:44 +0400

    correct indentation

--- week5/ex2_alt.c
+++ week5/ex2_alt.c
@@ -37,7 +37,7 @@ int main() {
 
 	for (int i = 1; i < n + 1; i++)
 		if (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])
-				printf("%d ", nums[i]);
+			printf("%d ", nums[i]);
 	
 	printf("\n");
 }
