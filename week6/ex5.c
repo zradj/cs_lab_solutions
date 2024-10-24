0c1fffe229f5d341090e59cf027850b646605568
Elnama <93837085+ielnama@users.noreply.github.com>
2024-10-25 00:51:07 +0400

    editing ex

--- week6/ex5.c
+++ week6/ex5.c
@@ -8,7 +8,7 @@
  *	the same backwards as forwards. E.g., “ana”, “ata”, 1991, “racecar”, etc. Given a
  *	sentence/word/number, print YES if that is palindrome, NO otherwise.
  *
- *  Solution:
+ *	Solution:
  *	Find the length of the given string.
  *	Declare a flag to track if the string is a palindrome.
  *	If the last character is '\n', decrement the length variable by 1 and update the termination position of the string.
@@ -17,34 +17,32 @@
  *	Print "YES" if the flag indicates the string is a palindrome, otherwise print "NO".
  */
 
-
 #include <stdio.h>
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-    char str[1002];
-    int length = 0;
-    int isPalindrome = 1;
+	char str[1002];
+	int length = 0;
+	int isPalindrome = 1;
 
-    fgets(str, sizeof(str), stdin);
-    length = strlen(str);
-    if (str[length - 1] == '\n') {
-        str[length - 1] = '\0';
-        length--;
-    }
-    for (int i = 0; i < length / 2; i++) {
-        if (str[i] != str[length - 1 - i]) {
-            isPalindrome = 0;
-            break;
-        }
-    }
+	fgets(str, sizeof(str), stdin);
+	length = strlen(str);
+	if (str[length - 1] == '\n') {
+		str[length - 1] = '\0';
+		length--;
+	}
+	for (int i = 0; i < length / 2; i++) {
+		if (str[i] != str[length - 1 - i]) {
+			isPalindrome = 0;
+			break;
+		}
+	}
 
-    if (isPalindrome)
-        printf("YES\n");
-    else
-        printf("NO\n");
+	if (isPalindrome)
+		printf("YES\n");
+	else
+		printf("NO\n");
 
-    return 0;
+	return 0;
 }
-
